c5969f81d4adb8433afd8d60c08bbbb4014c6c9a
Mike Roberts <mike@mpr.io>
2020-06-18 15:27:38 -0400

    test: basic builder run

--- lib/src/commonTest/kotlin/dev/mpr/grpc/BuilderTests.kt
+++ lib/src/commonTest/kotlin/dev/mpr/grpc/BuilderTests.kt
@@ -1,10 +1,34 @@
 package dev.mpr.grpc
 
+import com.google.protobuf.Timestamp
 import kotlin.test.Test
-import kotlin.test.assertTrue
+import kotlin.test.assertEquals
 
 class BuilderTests {
     @Test
-    fun testMe() {
+    fun `Build standard types`() {
+        val timestamp = Timestamp.build {
+            seconds = 1310090400
+        }
+
+        assertEquals(0, timestamp.nanos)
+        assertEquals(1310090400, timestamp.seconds)
+    }
+
+    @Test
+    fun `Modifying built object`() {
+        val timestamp = Timestamp.build {
+            seconds = 1310090400
+        }
+
+        val updatedTimestamp = timestamp.copyBuild {
+            nanos = 1
+        }
+
+        assertEquals(0, timestamp.nanos)
+        assertEquals(1310090400, timestamp.seconds)
+
+        assertEquals(1, updatedTimestamp.nanos)
+        assertEquals(1310090400, updatedTimestamp.seconds)
     }
 }
