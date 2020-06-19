9206f9f94d9e7310341d738bef8c2d3f84fba467
Mike Roberts <mike@mpr.io>
2020-06-18 23:12:47 -0400

    test: check deep one of builder usage

--- lib/src/commonTest/kotlin/dev/mpr/grpc/BuilderTests.kt
+++ lib/src/commonTest/kotlin/dev/mpr/grpc/BuilderTests.kt
@@ -1,6 +1,9 @@
 package dev.mpr.grpc
 
+import com.google.protobuf.NullValue
+import com.google.protobuf.Struct
 import com.google.protobuf.Timestamp
+import com.google.protobuf.Value
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
@@ -13,6 +16,41 @@ class BuilderTests {
 
         assertEquals(0, timestamp.nanos)
         assertEquals(1310090400, timestamp.seconds)
+    }
+
+    @Test
+    fun `Build bigger type`() {
+        val struct = Struct.build {
+            fields {
+                value("test1") {
+                    kind {
+                        boolValue = true
+                    }
+                }
+                value("test2") {
+                    kind {
+                        numberValue = 42.0
+                    }
+                }
+                value("test3") {
+                    kind {
+                        listValue {
+                            values {
+                                addValue {
+                                    kind {
+                                        nullValue = NullValue.NULL_VALUE
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        assertEquals(true, (struct.fields["test1"]?.kind as? Value.OneOfKind.boolValue)?.boolValue)
+        assertEquals(42.0, (struct.fields["test2"]?.kind as? Value.OneOfKind.numberValue)?.numberValue)
+        assertEquals(NullValue.NULL_VALUE, ((struct.fields["test3"]?.kind as? Value.OneOfKind.listValue)?.listValue?.values?.getOrNull(0)?.kind as? Value.OneOfKind.nullValue)?.nullValue)
     }
 
     @Test
